2ace823cf52f179bb14c2ef5e431001d002df767
Fermin Paez <39581462+FerminPaez@users.noreply.github.com>
2025-05-27 20:22:46 -0300

    Se arreglo problema de fechas

--- src/pages/dashboard/pages_dashboard/gestion_deudas/OpcionesDePago/OpcionesDePago.jsx
+++ src/pages/dashboard/pages_dashboard/gestion_deudas/OpcionesDePago/OpcionesDePago.jsx
@@ -1,4 +1,3 @@
-
 import {
   Box,
   Button,
@@ -16,10 +15,13 @@ import {
 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-//import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import moment from 'moment';
 import formatter from '@/common/formatter';
-import { generarConvenio } from '../Entidades/GestionApi';
-import { ThreeCircles } from 'react-loader-spinner';
+import { ThreeCircles } from 'react-loader-spinner';//import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import 'moment/locale/es'; // Importa el locale español de moment
+
+moment.locale('es');
 
 export const OpcionesDePago = ({
   cuotas,
@@ -27,21 +29,18 @@ export const OpcionesDePago = ({
   fechaIntencion,
   setFechaIntencion,
   medioPago,
+  setMedioPago,
   detalleConvenio,
   importeDeDeuda,
   saldoAFavorUtilizado,
   handleGenerarConvenio,
   showLoading
 }) => {
-
   return (
     <Box p={3} sx={{ margin: '60px auto', padding: 0 }}>
-      <Typography variant="h6" gutterBottom>
-        OPCIONES DE PAGO
-      </Typography>
+      <Typography variant="h6" gutterBottom>OPCIONES DE PAGO</Typography>
       <Typography variant="body2" gutterBottom>
-        IMPORTE CALCULADO TENIENDO EN CUENTA TODOS LOS PERIODOS ADEUDADOS Y LAS
-        ACTAS EMITIDAS
+        IMPORTE CALCULADO TENIENDO EN CUENTA TODOS LOS PERIODOS ADEUDADOS Y LAS ACTAS EMITIDAS
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12}>
@@ -64,11 +63,16 @@ export const OpcionesDePago = ({
             <Grid item xs={12} sm={5} md={4}>
               <FormControl fullWidth>
                 <FormLabel>Fecha de intención de pago de cuota 1</FormLabel>
-                <LocalizationProvider>
-                  <DatePicker
+                <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="es">
+                  <DatePicker 
+                    format="DD/MM/YYYY"
                     value={fechaIntencion}
                     onChange={(newValue) => setFechaIntencion(newValue)}
-                    renderInput={(params) => <TextField {...params} />}
+                    slotProps={{
+                      textField: {
+                        inputProps: { placeholder: 'DD/MM/YYYY' }
+                      }
+                    }}
                   />
                 </LocalizationProvider>
               </FormControl>
@@ -80,173 +84,86 @@ export const OpcionesDePago = ({
                   value={medioPago}
                   onChange={(e) => setMedioPago(e.target.value)}
                 >
-                  <FormControlLabel
-                    value="CHEQUE"
-                    control={<Radio />}
-                    label="CHEQUE"
-                  />
+                  <FormControlLabel value="CHEQUE" control={<Radio />} label="CHEQUE" />
                 </RadioGroup>
               </FormControl>
             </Grid>
           </Grid>
         </Grid>
-{/*
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                label="Intereses de financiación"
-                variant="outlined"
-                fullWidth
-                InputProps={{
-                  readOnly: true,
-                }}
-                value={formatter.currencyString(detalleConvenio.importeInteresTotal)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Saldo a favor"
-                variant="outlined"
-                fullWidth
-                InputProps={{
-                  readOnly: true,
-                }}
-                value={formatter.currencyString(saldoAFavorUtilizado * -1)}
-              />
-
-            </Grid>
-            <Grid item xs={12} marginTop={'2.3em'}>
-              <Typography variant="h6">TOTAL</Typography>
-              <TextField
-                variant="outlined"
-                fullWidth
-                InputProps={{
-                  readOnly: true,
-                }}
-                value={formatter.currencyString(importeDeDeuda + detalleConvenio.importeInteresTotal + saldoAFavorUtilizado)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              {!showLoading && (<Button variant="contained" color="primary" fullWidth onClick={() => handleGenerarConvenio()}>
-                GUARDAR CONVENIO
-              </Button>)}
-
-              <ThreeCircles
-                visible={showLoading}
-                height="100"
-                width="100"
-                color="#1A76D2"
-                ariaLabel="three-circles-loading"
-                wrapperStyle={{
-                  margin: '15%',
-                  marginLeft: '50%',
-                }}
-                wrapperClass=""
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-*/}
 
         <Grid item xs={12} md={12}>
           <Box border={1} p={2}>
             <Typography variant="h6">DETALLE DE CONVENIO</Typography>
             <Typography variant="body1">Ud. está generando un convenio con la siguiente información:</Typography>
-            <Box component="div" >
-              <Grid container spacing={2} sx={{ mb: 2 }}>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Typography variant="body2"><strong>Importe de deuda:</strong></Typography>
-                  <Typography variant="body1">{formatter.currencyString(importeDeDeuda)}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Typography variant="body2"><strong>Saldo a Favor utilizado:</strong></Typography>
-                  <Typography variant="body1">{formatter.currencyString(saldoAFavorUtilizado * -1)}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Typography variant="body2"><strong>Monto a financiar:</strong></Typography>
-                  <Typography variant="body1">{formatter.currencyString(importeDeDeuda + saldoAFavorUtilizado)}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Typography variant="body2"><strong>Cantidad de cuotas:</strong></Typography>
-                  <Typography variant="body1">{cuotas}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Typography variant="body2"><strong>Intereses de financiación:</strong></Typography>
-                  <Typography variant="body1">{formatter.currencyString(detalleConvenio.importeInteresTotal)}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <Typography variant="body2"><strong>Total a pagar:</strong></Typography>
-                  <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-                    {formatter.currencyString(importeDeDeuda + detalleConvenio.importeInteresTotal + saldoAFavorUtilizado)}
-                  </Typography>
-                  
-                </Grid>
+            <Grid container spacing={2} sx={{ mb: 2 }}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography variant="body2"><strong>Importe de deuda:</strong></Typography>
+                <Typography variant="body1">{formatter.currencyString(importeDeDeuda)}</Typography>
               </Grid>
-             {/*<ul>
-                
-                <li>Importe de deuda: {formatter.currencyString(importeDeDeuda)}</li>
-                <li>Saldo a Favor utilizado: {formatter.currencyString(saldoAFavorUtilizado * -1)}</li>
-                <li>Monto a financiar: {formatter.currencyString(importeDeDeuda + saldoAFavorUtilizado)}</li>
-                <li>Cantidad de cuotas: {cuotas}</li>
-                <li>Intereses de financiación: {formatter.currencyString(detalleConvenio.importeInteresTotal)}</li>
-                <li>Total a pagar: {formatter.currencyString(importeDeDeuda + detalleConvenio.importeInteresTotal + saldoAFavorUtilizado)}</li>
-                
-              </ul>*/}
-            </Box>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography variant="body2"><strong>Saldo a Favor utilizado:</strong></Typography>
+                <Typography variant="body1">{formatter.currencyString(saldoAFavorUtilizado * -1)}</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography variant="body2"><strong>Monto a financiar:</strong></Typography>
+                <Typography variant="body1">{formatter.currencyString(importeDeDeuda + saldoAFavorUtilizado)}</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography variant="body2"><strong>Cantidad de cuotas:</strong></Typography>
+                <Typography variant="body1">{cuotas}</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography variant="body2"><strong>Intereses de financiación:</strong></Typography>
+                <Typography variant="body1">{formatter.currencyString(detalleConvenio.importeInteresTotal)}</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography variant="body2"><strong>Total a pagar:</strong></Typography>
+                <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+                  {formatter.currencyString(importeDeDeuda + detalleConvenio.importeInteresTotal + saldoAFavorUtilizado)}
+                </Typography>
+              </Grid>
+            </Grid>
+
             <h4>Detalle cuotas</h4>
             <table>
               <thead>
                 <tr>
-                <th style={{ paddingRight: '64px' }}>Cuota Nº</th>
-                <th style={{ paddingRight: '64px' }}>Valor</th>
-                <th style={{ paddingRight: '64px' }}>Vencimiento</th>
-                  
+                  <th style={{ paddingRight: '64px' }}>Cuota Nº</th>
+                  <th style={{ paddingRight: '64px' }}>Valor</th>
+                  <th style={{ paddingRight: '64px' }}>Vencimiento</th>
                 </tr>
               </thead>
               <tbody>
-                {Array.from({ length: cuotas }, (_, i) => {
-                  //const cuota = (detalleConvenio.detalleCuota || [])[i];
-                  return (
-                    <tr key={i + 1}>
-                      <td className='pr2'>{i + 1}</td>
-                      <td className='pr2'>{formatter.currencyString((importeDeDeuda + detalleConvenio.importeInteresTotal + saldoAFavorUtilizado)/cuotas)}</td>
-                      {/* Calcular la fecha de vencimiento sumando i meses a la fecha de intención */}
-                      <td className='pr2'>
-                        {fechaIntencion
-                          ? fechaIntencion.clone().add(i, 'month').format("DD/MM/YYYY")
-                          : '-'}
-                      </td></tr>
-                  );
-                })}
-                {/*(detalleConvenio.detalleCuota || []).map((cuota) => (
-                  <tr key={cuota.numero}>
-                    <td className='pr2'>{cuota.numero}</td>
-                    <td className='pr2'>{formatter.currencyString(cuota.valor)}     </td>
-                    <td className='pr2'>{cuota.vencimiento}</td>
+                {Array.from({ length: cuotas }, (_, i) => (
+                  <tr key={i + 1}>
+                    <td className='pr2'>{i + 1}</td>
+                    <td className='pr2'>{formatter.currencyString((importeDeDeuda + detalleConvenio.importeInteresTotal + saldoAFavorUtilizado) / cuotas)}</td>
+                    <td className='pr2'>
+                      {fechaIntencion && moment(fechaIntencion).isValid()
+                        ? moment(fechaIntencion).clone().add(i, 'month').format("DD/MM/YYYY")
+                        : '–'}
+                    </td>
                   </tr>
-                ))*/}
+                ))}
               </tbody>
             </table>
-          </Box>
-           <Grid item xs={12}>
-              {!showLoading && (<Button variant="contained" color="primary" fullWidth onClick={() => handleGenerarConvenio()}>
-                GUARDAR CONVENIO
-              </Button>)}
 
+            <Grid item xs={12}>
+              {!showLoading && (
+                <Button variant="contained" color="primary" fullWidth onClick={handleGenerarConvenio}>
+                  GUARDAR CONVENIO
+                </Button>
+              )}
               <ThreeCircles
                 visible={showLoading}
                 height="100"
                 width="100"
                 color="#1A76D2"
                 ariaLabel="three-circles-loading"
-                wrapperStyle={{
-                  margin: '15%',
-                  marginLeft: '50%',
-                }}
-                wrapperClass=""
+                wrapperStyle={{ margin: '15%', marginLeft: '50%' }}
               />
             </Grid>
+          </Box>
         </Grid>
       </Grid>
     </Box>
